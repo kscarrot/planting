4c98d42cbe326c4ecd21db971fb6d7dc84b3fb3f
kscarrot <kelstrait@icloud.com>
2019-09-15 15:57:36 +0800

    test: update test

--- src/datastructure/list/list.test.ts
+++ src/datastructure/list/list.test.ts
@@ -6,12 +6,18 @@ type NumberList = LinkedList<number> | DoublyLinkedList<number>
 const testInit = (l: NumberList) => {
   //test init
   expect(l.length).toBe(0)
+  expect(l.head).toBe(null)
+  expect(l.tail).toBe(null)
   //add one element than delete
   l.insert(0, 1)
   expect(l.length).toBe(1)
   expect(l.get(0)).toBe(1)
+  expect(l.getNode(0)).toBe(l.head)
+  expect(l.getNode(0)).toBe(l.tail)
   l.delete(0)
   expect(l.isEmpty()).toBe(true)
+  expect(l.head).toBe(null)
+  expect(l.tail).toBe(null)
 }
 
 const testBoundary = (l: NumberList) => {
@@ -20,11 +26,15 @@ const testBoundary = (l: NumberList) => {
   l.insert(0, 1)
   l.insert(2, 3)
   expect(() => l.insert(-1, 2)).toThrow()
+  expect(l.getNode(0)).toBe(l.head)
+  expect(l.getNode(2)).toBe(l.tail)
   expect(l.length).toBe(3)
   //delete head and tail
   l.delete(2)
   l.delete(0)
   expect(l.getNode(0).value).toBe(2)
+  expect(l.getNode(0)).toBe(l.head)
+  expect(l.getNode(0)).toBe(l.tail)
   l.delete(0)
   expect(l.isEmpty()).toBe(true)
 }
@@ -34,7 +44,6 @@ const testTraverse = (l: NumberList) => {
   arr.map(e => l.add(e))
   expect([...l]).toStrictEqual(arr)
   l.insert(3, 9)
-  expect([...l]).toStrictEqual([0, 1, 2, 9, 3, 4])
   l.delete(2)
   expect([...l]).toStrictEqual([0, 1, 9, 3, 4])
 }
@@ -51,5 +60,20 @@ test('test LinkedList boundary case', () => {
 
 test('test LinkedList traverse', () => {
   const l = new LinkedList<number>()
+  testTraverse(l)
+})
+
+test('test DoublyLinkedList init', () => {
+  const l = new DoublyLinkedList<number>()
+  testInit(l)
+})
+
+test('test DoublyLinkedList boundary case', () => {
+  const l = new DoublyLinkedList<number>()
+  testBoundary(l)
+})
+
+test('test DoublyLinkedList traverse', () => {
+  const l = new DoublyLinkedList<number>()
   testTraverse(l)
 })
