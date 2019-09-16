f9ea12e56f82628bc05598c22078ab1e195f3172
ks <kelstrait@icloud.com>
2019-09-16 23:29:45 +0800

    fix: fix ci bugs-2

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils')
+const { compilerOptions } = require('./tsconfig')
+
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   testPathIgnorePatterns: ['node_modules', 'build'],
-  moduleNameMapper: { '@ds/(.*)': '<rootDir>/src/datastructure/$1' },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src/' }),
 }
