14b727bcd156c64416f36c05905bc856306f602b
kscarrot <kelstrait@icloud.com>
2020-06-12 10:42:39 +0800

    feat: update package version (#38)

--- src/algorithm/sort/bubbleSort.ts
+++ src/algorithm/sort/bubbleSort.ts
@@ -1,11 +1,11 @@
 import { cmp } from './index'
 function bubbleSort<T>(nums: T[]) {
-  if (nums.length === 0) {
-    return []
+  if (nums.length === 0 || nums.length === 1) {
+    return nums
   }
   let flag = true
   let range = nums.length - 1
-  while (flag && range !== 0) {
+  while (flag) {
     flag = false
     for (let i = 0; i < range; i++) {
       if (cmp.gt(nums[i], nums[i + 1])) {
